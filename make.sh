d25b04e633a1f0dfcb8bb9759e2172f4211caac8
John Carlson <yottzumm@gmail.com>
2017-01-03 23:07:07 -0500

    Updated

--- make.sh
+++ make.sh
@@ -1,18 +1,16 @@
-#!/usr/bin/bash
+#!/bin/bash
 . ./profile.sh
 
 ${PYTHON} classes.py
-cp X3Dautoclass.py ${PYJHOME}/../pyjnius
+cp X3Dautoclass.py ${PYJHOME}/pyjnius
+mkdir -p ${PYJHOME}/pyjnius/X3DJSONLD
 
 cd ../X3DJSONLD
-FILES=`sh runjson.sh ${NODE} | xargs -0 echo | tr ' ' '\n' | grep '\.py'`
-echo $FILES
-mkdir -p ${PYJHOME}/../pyjnius/X3DJSONLD
 
-cd ../pyjnius
-for i in $FILES
+for i in `sh runjson.sh | grep '\.py'`
 do
+	cd ../pyjnius
 	mkdir -p X3DJSONLD/`dirname $i`
-	cp ${PYJHOME}/../X3DJSONLD/$i X3DJSONLD/`dirname $i`
+	cp ${PYJHOME}/X3DJSONLD/$i X3DJSONLD/`dirname $i`
 	${PYTHON} X3DJSONLD/`dirname $i`/`basename $i` && echo "$i passed"
 done
--- profile.sh
+++ profile.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-PYJHOME=.
-export CLASSPATH=${PYJHOME}/../pythoncode/jslint4java-2.0.5.jar:${PYJHOME}/../pythoncode/json-schema-validator-2.2.6-lib.jar:${PYJHOME}/../pythoncode/X3dJavaSceneAccessInterface3.3.classes.jar:${PYJHOME}/../pythoncode/X3dJavaSceneAccessInterface3.3.full.jar
-export CLASSPATH=${PJYHOME}/../pyjnius/build/classes:${CLASSPATH}
-export PYTHONPATH=${PJYHOME}/../pyjnius:${PYTHONPATH}
+PYJHOME=/home/coderextreme
+export PATH=/home/coderextreme/node-v7.3.0-linux-x64/bin:/usr/lib/jvm/java-8-openjdk-amd64/jre/lib/amd64/server:${PATH}
+export CLASSPATH=${PYJHOME}/pythonSAI/jslint4java-2.0.5.jar:${PYJHOME}/pythonSAI/json-schema-validator-2.2.6-lib.jar:${PYJHOME}/pythonSAI/saxon9he.jar:${PYJHOME}/pythonSAI/X3dJavaSceneAccessInterface3.3.classes.jar:${PYJHOME}/pythonSAI/X3dJavaSceneAccessInterface3.3.full.jar
+# export CLASSPATH=${PYJHOME}/pyjnius/build/classes:${CLASSPATH}
+export PYTHONPATH=${PYJHOME}/pyjnius:${PYTHONPATH}
 export PYTHON=python3
-export NODE=nodejs
+export NODE=node
