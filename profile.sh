df18c82b92cb2258ddd2524fb83691dd2849ad6d
John Carlson <yottzumm@gmail.com>
2017-03-30 15:14:18 -0400

    modified class path

--- profile.sh
+++ profile.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 PYJHOME=/home/coderextreme
 export PATH=/home/coderextreme/node-v7.3.0-linux-x64/bin:/usr/lib/jvm/java-8-openjdk-amd64/jre/lib/amd64/server:${PATH}
-export CLASSPATH=${PYJHOME}/pythonSAI/jslint4java-2.0.5.jar:${PYJHOME}/pythonSAI/json-schema-validator-2.2.6-lib.jar:${PYJHOME}/pythonSAI/saxon9he.jar:${PYJHOME}/pythonSAI/X3dJavaSceneAccessInterface3.3.classes.jar:${PYJHOME}/pythonSAI/X3dJavaSceneAccessInterface3.3.full.jar
+export CLASSPATH=${PYJHOME}/pythonSAI/jslint4java-2.0.5.jar:${PYJHOME}/pythonSAI/json-schema-validator-2.2.6-lib.jar:${PYJHOME}/pythonSAI/saxon9he.jar:${PYJHOME}/pythonSAI/X3DJSAIL.3.3.classes.jar:${PYJHOME}/pythonSAI/X3DJSAIL.3.3.full.jar
 # export CLASSPATH=${PYJHOME}/pyjnius/build/classes:${CLASSPATH}
 export PYTHONPATH=${PYJHOME}/pyjnius:${PYTHONPATH}
 export PYTHON=python3
