0574723a5b5df7ee51e157b2355102d38f6786c3
John Carlson <yottzumm@gmail.com>
2017-05-06 03:30:15 -0400

    Added x3d2py.bat and clean.sh

---
+++ x3d2py.bat
@@ -0,0 +1,18 @@
+REM accepts files with .x3d extension
+
+@ECHO OFF
+set PYSAIHOME=.
+set X3DJSONLD=..\X3DJSONLD
+set PYTHONPATH=C:\Python27;c:\Python27\Scripts
+set PATH=%PYTHONPATH%;%JDK_HOME%\bin:%JDK_HOME%\jre\lib\amd64\server:%PATH%
+set CLASSPATH=.;%PYSAIHOME%\saxon9he.jar;%PYSAIHOME%\X3DJSAIL.3.3.classes.jar
+set PYTHON=python.exe
+set PIP=pip.exe
+set NODE=node.exe
+@ECHO ON
+
+javac RunSaxon.java
+
+java RunSaxon ---overwrite *.x3d
+FOR %%i IN ("*.json") DO %NODE% json2py.js %%i
+
