d674920829ea1069d99a8f740d8340900491bc7a
John Carlson <yottzumm@gmail.com>
2017-05-19 05:45:28 -0400

    Checked in new jar

--- profile.sh
+++ profile.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 export PYTHON=python
 export PIP=pip
-export NODE=nodejs
-export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/
-export JDK_HOME=/usr/lib/jvm/java-8-openjdk-amd64/
+export NODE=node
+export JAVA_HOME="/C/Program Files/Java/jdk1.8.0_121"
+export JDK_HOME="/C/Program Files/Java/jdk1.8.0_121"
 export PYSAIHOME=.
 export PYTHONPATH=/usr/bin:/c/Python27:/c/Python27/Scripts
-export PATH=${PYTHONPATH}:${JAVA_HOME}/bin:${JAVA_HOME}/jre/lib/amd64/server:${PATH}
-export CLASSPATH=".:${PYSAIHOME}/saxon9he.jar:${PYSAIHOME}/X3DJSAIL.3.3.classes.jar"
+export PATH="${PYTHONPATH};${JAVA_HOME}/bin;${JAVA_HOME}/jre/lib/amd64/server;${PATH}"
+export CLASSPATH=".;${PYSAIHOME}/saxon9he.jar;${PYSAIHOME}/X3DJSAIL.3.3.classes.jar"
