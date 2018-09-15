d07ec1630cadcac671d1239a7e94ee7f6aceff23
John Carlson <yottzumm@gmail.com>
2018-09-14 20:17:10 -0400

    modded

--- HelloWorldProgramOutput.py
+++ HelloWorldProgramOutput.py
@@ -877,4 +877,4 @@ Shape143.setGeometry(Extrusion146)
 Scene26.addChild(Shape143)
 X3D0.setScene(Scene26)
 
-X3D0.toFileX3D("././HelloWorldProgramOutput.new.x3d")
+X3D0.toFileX3D("./HelloWorldProgramOutput.new.x3d")
