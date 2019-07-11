cd8f6bcead5532900834e2b7f785d293a8659bbc
John Carlson <yottzumm@gmail.com>
2019-07-10 20:19:55 -0500

    changed filenames

--- clean.sh
+++ clean.sh
@@ -2,4 +2,4 @@
 
 # clean up test files
 
-rm -f `ls *py | grep -v classes.py | grep -v abox.py | grep -v fieldtypes.py | grep -v old.py | grep -v parseom.py` *.sail.js *.x3d.new `ls *.class *.java | grep -v RunSaxon` *.intermediate.* *.diff *.x3d*.json *three.js *.bak *roundtrip* *Canonical* *.run*.txt [0-9]* *.new.json `ls *.json | grep -v Schema | grep -v package.json`
+rm -f `ls *py | grep -v classes.py | grep -v abox.py | grep -v fieldTypesGenerator.py | grep -v old.py | grep -v mapToMethodGenerator.py` *.sail.js *.x3d.new `ls *.class *.java | grep -v RunSaxon` *.intermediate.* *.diff *.x3d*.json *three.js *.bak *roundtrip* *Canonical* *.run*.txt [0-9]* *.new.json `ls *.json | grep -v Schema | grep -v package.json`
