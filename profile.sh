08e1cb4089b528209f1ea3699340e84f9baa85bf
John Carlson <yottzumm@gmail.com>
2020-04-20 00:32:24 -0500

    Upgrade to JDK14, python 3.8

--- profile.sh
+++ profile.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 export PYTHON=python
-export PIP=pip3
+export PIP=pi
 export NODE=node
-export JAVA_HOME="/c/Program Files/Java/jdk13.0.1/jdk-13.0.1"
-export JDK_HOME="/c/Program Files/Java/jdk13.0.1/jdk-13.0.1"
+export JAVA_HOME="/c/Program Files/Java/jdk-14.0.1"
+export JDK_HOME="/c/Program Files/Java/jdk-14.0.1"
 
 export PYSAIHOME=.
-export PYTHONPATH=/c/Users/coderextreme/AppData/Local/Programs/Python/Python36/:/c/Users/coderextreme/AppData/Local/Programs/Python/Python36/Scripts:/usr/bin:/c/Python27:/c/Python27/Scripts
+export PYTHONPATH=/c/Users/coderextreme/AppData/Local/Programs/Python/Python38/:/c/Users/coderextreme/AppData/Local/Programs/Python/Python38/Scripts:/usr/bin:/c/Python27:/c/Python27/Scripts
 
 
-export PATH="${PYTHONPATH};${JAVA_HOME}/bin;${PATH}"
+export PATH="${PYTHONPATH};${JAVA_HOME}/bin;${PATH}:/c/Program\ Files/Java/jdk-14.0.1/bin/server"
 export CLASSPATH=".;${PYSAIHOME}/saxon9he.jar;${PYSAIHOME}/X3DJSAIL.3.3.full.jar"
